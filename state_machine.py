66ce481e280001332c67410d043935f1146547d9
mputnieks <59909688+mputnieks@users.noreply.github.com>
2023-10-26 15:31:28 +0200

    removed unnecessary code

--- state_machine.py
+++ state_machine.py
@@ -5,12 +5,10 @@ from state_functions import StateFunctions
 
 class StateMachine(object):
     def __init__(self, ticker_frequency):
-        # self.serial_pc = SerialPC(3) # serial used for monotoring, passed to self.sensor_state to have just 1 serial where everything is plotted
         self.robot_state = State()
         self.sensor_state = SensorState()
         self.state_functions = StateFunctions(self.robot_state, self.sensor_state, ticker_frequency)
         self.ticker = Ticker(0, ticker_frequency, self.run) #1 number on hardware timer, 2 frequency, 3 function you want to run
-        
         return
     
     # Function that is run by the Ticker at the ticker_frequency
