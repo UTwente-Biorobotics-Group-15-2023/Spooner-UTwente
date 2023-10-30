f3ec804bac584248602625c3aa5ae9154e5b1ab9
Viktor <v.kulicgolja@student.utwente.nl>
2023-10-30 10:54:02 +0100

    Fixed encoderstats.py to output in rad.

--- encoderstats.py
+++ encoderstats.py
@@ -1,5 +1,6 @@
 from biorobotics import Encoder
 from pin_definitions import Pins
+from ulab import numpy as np
 
 
 class EncoderStats(object):
@@ -12,12 +13,14 @@ class EncoderStats(object):
             pin_2 = Pins.MOTOR_2_ENCODER_2
 
         self.encoder = Encoder(pin_1, pin_2)
+        self.angle_conversion_constant = (np.pi / 180)
         return
 
     def get_angle(self):
         self.count = self.encoder.counter() % 8400
         self.angle = self.count * 360 / 8400
-        return self.angle
+        self.angle_radians = self.angle * self.angle_conversion_constant
+        return self.angle_radians
 
     def get_angular_velocity(self):
 
